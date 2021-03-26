f1345f40a1d9deb204a6c1a2874e06c185cae0e5
Robert <1robertchambers@gmail.com>
2021-03-26 16:04:58 +0000

    docs: add docstring for entry point

--- periodic_scraper/main.py
+++ periodic_scraper/main.py
@@ -1,7 +1,11 @@
+"""
+main should be set as the entry point for Google Cloud function
+"""
 import periodic_scraper as ps
 
 
 def main(data, context):
+    print("debug in main")
     reasonable_mp_update_frequency = 7
     ps.insert_and_update_data(day_frequency_for_party_and_mp_data=reasonable_mp_update_frequency)
 
